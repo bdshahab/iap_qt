8b2b8f380fc9291016efee22b7bc413007607e74
Shahab Baradaran Dilmaghani <bdshahab@gmail.com>
2025-03-24 00:13:59 +0330

    Update select_coin.py

--- Payment/select_coin.py
+++ Payment/select_coin.py
@@ -1,28 +1,14 @@
-# -*- coding: utf-8 -*-
+from PySide6.QtCore import (QCoreApplication, QMetaObject, Qt)
+from PySide6.QtGui import (QIcon)
+from PySide6.QtWidgets import (QDialog, QHBoxLayout, QLabel,
+                               QPushButton, QSizePolicy, QVBoxLayout, QMessageBox)
 
-################################################################################
-# Form generated from reading UI file 'select_coinWgeuZj.ui'
-##
-# Created by: Qt User Interface Compiler version 6.8.2
-##
-# WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-                            QMetaObject, QObject, QPoint, QRect,
-                            QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-                           QFont, QFontDatabase, QGradient, QIcon,
-                           QImage, QKeySequence, QLinearGradient, QPainter,
-                           QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QHBoxLayout, QLabel,
-                               QPushButton, QSizePolicy, QVBoxLayout, QWidget, QMessageBox)
-from tools.for_images import show_image
 import Global
+from Payment.payment import Ui_Payment
 from tools.Centralization import center_window
-from tools.iap_variables import TITLE_COIN_NOT_SELECTED, MESSAGE_COIN_NOT_SELECTED
-from Payment.payment import MINIMUM_LIMIT_PRICE, Ui_Payment
 from tools.dialogue import show_the_message
+from tools.for_images import show_image
+from tools.iap_variables import TITLE_COIN_NOT_SELECTED, MESSAGE_COIN_NOT_SELECTED
 
 
 class Ui_Select_Coin(QDialog):
