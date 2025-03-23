127875ae0e9e9d8e22116f443a77bfb1b7b6d3c0
Shahab Baradaran Dilmaghani <bdshahab@gmail.com>
2025-03-24 00:15:27 +0330

    Update payment.py

--- Payment/payment.py
+++ Payment/payment.py
@@ -1,26 +1,16 @@
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-                            QMetaObject, QObject, QPoint, QRect,
-                            QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-                           QFont, QFontDatabase, QGradient, QIcon,
-                           QImage, QKeySequence, QLinearGradient, QPainter,
-                           QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QHBoxLayout, QLabel, QLineEdit,
-                               QPushButton, QSizePolicy, QVBoxLayout, QWidget, QMessageBox)
+from PySide6.QtCore import (QCoreApplication, QMetaObject)
 from PySide6.QtCore import QTimer, QElapsedTimer
-
-import re
-import requests
+from PySide6.QtWidgets import (QApplication, QDialog, QHBoxLayout, QLineEdit,
+                               QSizePolicy, QVBoxLayout, QMessageBox)
 
 import Global
-from tools.dialogue import show_the_message
+import Payment.addresses as addr
+from Payment.web_functions import *
 from tools.Centralization import center_window
+from tools.dialogue import show_the_message
 from tools.for_images import *
 from tools.for_time import *
 from tools.iap_variables import *
-from Payment.web_functions import *
-
-import Payment.addresses as addr
 
 first_clock_now = ""
 last_clock_now = ""
