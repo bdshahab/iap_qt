a8f63f2732db55b8dba1d632acc50ca1c78653f4
Shahab Baradaran Dilmaghani <bdshahab@gmail.com>
2025-05-16 09:41:31 +0330

    Update Main_Menu.py

--- Main_Menu.py
+++ Main_Menu.py
@@ -161,9 +161,6 @@ def loading_heavy_modules_for_fast_loading():
     from About.about import Ui_About
     from Payment.select_coin import Ui_Select_Coin
     from Payment.bought import Ui_Bought
-    Ui_About()
-    Ui_Select_Coin()
-    Ui_Bought()
 
 
 if __name__ == "__main__":
