189fcef2f3d1cd57507fcee588af496476a0b63c
Shahab Baradaran Dilmaghani <bdshahab@gmail.com>
2025-05-20 13:51:37 +0330

    Update Centralization.py

--- tools/Centralization.py
+++ tools/Centralization.py
@@ -1,24 +1,10 @@
 from PySide6.QtWidgets import QApplication
-import os
-import sys
 
 
-# set window in the center
-def center_window(window):  # type: ignore
-    sys.path.append(os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..')))
-    import Global
-
-    screen_geometry = QApplication.primaryScreen().availableGeometry()
-    Global.screen_height = screen_geometry.height()
-    Global.screen_width = screen_geometry.width()
-
-    if Global.screen_height < Global.screen_width:
-        Global.img_size = Global.screen_height // 8
-    else:
-        Global.img_size = Global.screen_width // 8
-
-    # Resize the window to 75% of screen size
-    new_width = int(Global.screen_width * 0.75)
-    new_height = int(Global.screen_height * 0.75)
-    window.resize(new_width, new_height)  # type: ignore
+def center_window(window):
+    screen = QApplication.primaryScreen()
+    screen_geometry = screen.availableGeometry()
+    window_geometry = window.frameGeometry()
+    center_point = screen_geometry.center()
+    window_geometry.moveCenter(center_point)
+    window.move(window_geometry.topLeft())
